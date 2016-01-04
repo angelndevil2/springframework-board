0a3cfc66188010de591962437e87269fb3bd8fdd
k <jhkim@aimtog.co.kr>
2016-01-04 16:17:47 +0900

    apply gradle wrapper

---
+++ src/main/java/com/and/springframework/board/Greeter.java
@@ -0,0 +1,19 @@
+package com.and.springframework.board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author k, Created on 15. 12. 29.
+ */
+@Controller
+public class Greeter {
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
